1eb03d08c529857383c430d21aa5bc5ff8229d72
livillaranda <livillaranda@gmail.com>
2020-12-21 18:42:59 -0500

    Update to assignment.

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,8 +2,7 @@
 var myMap = L.map("map", {
     center: [41.0017, -104.0532],
     // center: [37.5452, -77.5407], // University of Richmond
-    zoom: 3,
-    layers: [lightMap]
+    zoom: 4.5,
 });
 
 // Light Map
@@ -11,7 +10,7 @@ var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     maxZoom: 18,
     id: "mapbox/light-v10",
-    accessToken: LI_MAPBOX
+    accessToken: API_KEY
 }).addTo(myMap);
 
 var site = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -29,22 +28,19 @@ d3.json(site).then(function (response) {
     for (var i = 0; i < response["features"].length; i++) {
 
         // Set Location
-        var earthquake = response["features"][i].geometry[1];
+        var earthquake = response["features"][i].geometry;
 
-        console.log(earthquake)
+        console.log(earthquake.coordinates);
 
-        // // Check for location property
-        // if (location) {
+        // Check for location property
+        if (earthquake) {
 
-        //     // Add a new marker to the cluster group and bind a pop-up
-        //     markers.addLayer(L.marker([location.coordinates[0], location.coordinates[1]]))
-        // };
+            // Add a new marker to the cluster group and bind a pop-up
+            markers.addLayer(L.marker([earthquake.coordinates[1], earthquake.coordinates[0]]))
+        };
 
     }
 
     // Add our marker cluster layer to the map
-    // myMap.addLayer(markers);
-
-    console.log("IS THIS WORKING?");
-
+    myMap.addLayer(markers);
 });
